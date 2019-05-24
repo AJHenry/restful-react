6666704480ca2f07fe1c5df27f39185cdac9c9b5
Andrew Henry <imandrewh@outlook.com>
2019-05-24 11:03:13 -0400

    Added fix for installing from github

---
+++ lib/util/cache.d.ts
@@ -0,0 +1,6 @@
+declare const setCache: (key: object, value: any, timeout: number | undefined) => void;
+declare const clearCache: (key: object) => void;
+declare const getCache: (key: object, def?: any) => any;
+
+export { setCache, clearCache, getCache };
+// # sourceMappingURL=cache.d.ts.map
